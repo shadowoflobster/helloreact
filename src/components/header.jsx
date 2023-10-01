fa83670bb80519d0e125d7a8b301e9b03f1c7b43
shadowoflobster <68239409+shadowoflobster@users.noreply.github.com>
2023-10-01 16:49:31 +0400

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -50,7 +50,7 @@ export const Header = () => {
                       </a>
                       <a href="#">
                         <li>
-                          სახელოსნო
+                          სახ ელოსნო
                         </li>
                       </a>
                     </ul>
